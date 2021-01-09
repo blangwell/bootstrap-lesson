a8a72ba3d6aff69747053e95ff10e529953b4618
blangwell <b.langwell@outlook.com>
2021-01-09 15:24:58 -0800

    First commit - set up layout, configure app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const PORT = 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(ejsLayouts);
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public/'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen(PORT, () => console.log(`listenin' on port ${PORT}`));
